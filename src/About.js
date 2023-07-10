f88a0ef6feb02f45deaa58a0de66cb30d6084a3b
Hafid Eledath <hafideledath@gmail.com>
2023-07-10 19:11:23 +0400

    Changes to metadata

--- src/About.js
+++ src/About.js
@@ -6,7 +6,7 @@ function About() {
     <div className="About">
       <h1 className='heading-shadow mb-10'>Why DIAPAIR?</h1>
       <div className="flex justify-evenly items-center info-container h-min">
-        <h6 className='white text-center lg:text-2xl border-dashed border-white border-2 rounded-xl w-1/4 p-10 h-full'>DIAPAIR is a student-connect platform that allows for DIA students of varied experience and age to collaborate with peers.</h6>
+        <h6 className='white text-center lg:text-2xl border-dashed border-white border-2 rounded-xl w-1/4 p-10 h-full moving-border'>DIAPAIR is a student-connect platform that allows for DIA students of varied experience and age to collaborate with peers.</h6>
         <Tabs />
       </div>
     </div>
