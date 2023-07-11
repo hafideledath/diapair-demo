fb8b8204ceda7b57d852b2e8f3f8fa845e2ace0f
Hafid Eledath <hafideledath@gmail.com>
2023-07-11 06:08:37 +0400

    Aesthetic modifications to dashboard

--- src/Dashboard.js
+++ src/Dashboard.js
@@ -7,13 +7,18 @@ const Dashboard = ({ displayName }) => {
   return (
     <div className="Dashboard">
       <h1 className='heading-shadow'>Good {daystate}!</h1>
-      <div className="flex gap-4 w-screen h-min sm:px-5 md:px-20 lg:px-40 border-box mb-5">
+      <div className="flex gap-4 w-screen h-min sm:px-5 md:px-20 lg:px-40 border-box mb-5 user">
         <div className="rounded-xl module lg:w-1/2 p-8 border-box text-center sm:text-2xl lg:text-3xl font-medium">
           <h4><b>{displayName}</b></h4>
           <h4>Class of 2026</h4>
         </div>
         <div className="rounded-xl module w-5/4 p-6 border-box max-h-[250px] overflow-hidden">
-          <p className='sm:text-xl lg:text-2xl text-ellipses'>Lorem ipsum dolor sit amet consectetur adipisicing elit. Ipsum, doloremque! Quos nobis culpa nisi nostrum at, magni sed maiores perferendis.</p>
+          <p className='sm:text-xl lg:text-2xl text-ellipses'>Lorem ipsum dolor sit amet consectetur adipisicing elit.</p>
+          <div className="flex">
+            <div className="tag">Compsci</div>
+            <div className="tag">Art</div>
+            <div className="tag">Linguistics</div>
+          </div>
         </div>
       </div>
 
@@ -21,9 +26,14 @@ const Dashboard = ({ displayName }) => {
         <p className='text-xl lg:text-2xl text-center'>You are currently paired with <b>John Doe</b>. Your chat is now active.</p>
       </div>
 
-      <div className="flex gap-4 w-screen h-min sm:px-5 md:px-20 lg:px-40 border-box mb-5">
+      <div className="flex gap-4 w-screen h-min sm:px-5 md:px-20 lg:px-40 border-box mb-5 paired-with">
       <div className="rounded-xl module w-5/4 p-6 border-box max-h-[250px] overflow-hidden">
-          <p className='sm:text-xl lg:text-2xl text-ellipses'>Lorem ipsum dolor sit amet consectetur adipisicing elit. Ipsum, doloremque! Quos nobis culpa nisi nostrum at, magni sed maiores perferendis.</p>
+          <p className='sm:text-xl lg:text-2xl text-ellipses'>Lorem ipsum dolor sit amet consectetur adipisicing elit.</p>
+          <div className="flex">
+            <div className="tag">Compsci</div>
+            <div className="tag">ML</div>
+            <div className="tag">Photography</div>
+          </div>
         </div>
         <div className="rounded-xl module lg:w-1/2 p-8 border-box text-center sm:text-2xl lg:text-3xl font-medium">
           <h4><b>John Doe</b></h4>
